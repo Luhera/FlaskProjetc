4db357918721915e2f4ef6d5759951d4074e8bc6
Anderson  Henrique Lucena Do Nascismento Heraclio <alucena933@gmail.com>
2024-07-24 19:06:32 -0300

    primeira aula

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Teste")
+from flask import Flask 
+
+app = Flask ( __name__)
+
+@app.route("/")
+def index():
+  return "Olá"
+
+@app.route("/teste")
+def teste():
+  return "Teste"
+
+if __name__ ==" __main__":
+  app.run()
